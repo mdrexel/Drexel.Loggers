3a0011d84b91d8263133df7d1809de12b3530f0d
mdrexel <max-github@drexel.dev>
2022-03-17 15:34:04 -0700

    Make a fancy base class

---
+++ src/Drexel.Loggers/ILocalizableString.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Drexel.Loggers
+{
+    /// <summary>
+    /// Represents a localizable string. A localizable string is a mapping between cultures and strings, where all
+    /// strings have the same semantic meaning.
+    /// </summary>
+    /// <typeparam name="TSelf">
+    /// A type that implements this interface using the same type parameters as the implementor of this interface.
+    /// </typeparam>
+    /// <typeparam name="TDerived">
+    /// A type that derives from <typeparamref name="TSelf"/>.
+    /// </typeparam>
+    public interface ILocalizableString<TSelf, TDerived> : IEquatable<TDerived?>, IComparable<TDerived?>
+        where TSelf : ILocalizableString<TSelf, TDerived>
+        where TDerived : class, TSelf
+    {
+        /// <summary>
+        /// Gets the preferred culture of this instance.
+        /// </summary>
+        /// <remarks>
+        /// The preferred culture is the culture that will be used when an operation performed on this instance
+        /// requires culture information, but the operation does not specify (or is unable to specify) a culture.
+        /// </remarks>
+        CultureInfo PreferredCulture { get; }
+
+        /// <summary>
+        /// Gets this instance's mappings between cultures and strings.
+        /// </summary>
+        /// <value>
+        /// A dictionary that is guaranteed to contain a value for this instance's <see cref="PreferredCulture"/>.
+        /// </value>
+        IReadOnlyDictionary<CultureInfo, string> Localizations { get; }
+
+        /// <summary>
+        /// Compares this instance with the instance specified by <paramref name="other"/> and returns an integer that
+        /// indicates whether this instance precedes, follows, or occurs in the same position in the sort order as the
+        /// other instance.
+        /// </summary>
+        /// <param name="other">
+        /// The instance to compare this instance to.
+        /// </param>
+        /// <param name="culture">
+        /// The culture to perform the comparison under. If <see langword="null"/>, the instances will be compared
+        /// using their preferred cultures. If an instance does not have a localization in the specified culture, the
+        /// comparison will use the localization in that instance's preferred culture.
+        /// </param>
+        /// <param name="options">
+        /// An optional value indicating the options that should be used by the comparison.
+        /// </param>
+        /// <returns>
+        /// <list type="bullet">
+        /// <item>A negative value, when this instance precedes the specified instance.</item>
+        /// <item>Zero, when this instance occurs in the same position as the specified instance.</item>
+        /// <item>A positive value, when this instance follows the specified instance, or <paramref name="other"/> is
+        /// <see langword="null"/>.</item>
+        /// </list>
+        /// </returns>
+        int CompareTo(TDerived? other, CultureInfo? culture, CompareOptions options = CompareOptions.None);
+
+        /// <summary>
+        /// Returns a value indicating whether the instance specified by <paramref name="obj"/> is equal to this
+        /// instance in the culture specified by <paramref name="culture"/>.
+        /// </summary>
+        /// <param name="obj">
+        /// The instance to compare this instance to.
+        /// </param>
+        /// <param name="culture">
+        /// The culture to perform the comparison under. If <see langword="null"/>, the instances will be compared
+        /// using their preferred cultures. If an instance does not have a localization in the specified culture, the
+        /// comparison will use the localization in that instance's preferred culture.
+        /// </param>
+        /// <param name="options">
+        /// An optional value indicating the options that should be used by the comparison.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if this instance is equal to the object specified by <paramref name="obj"/> in the
+        /// culture specified by <paramref name="culture"/> according to the specified options; otherwise,
+        /// <see langword="false"/>.
+        /// </returns>
+        bool Equals(TDerived? other, CultureInfo? culture, CompareOptions options = CompareOptions.None);
+
+        /// <summary>
+        /// Returns a value indicating whether the object specified by <paramref name="obj"/> is equal to this instance
+        /// in the culture specified by <paramref name="culture"/>.
+        /// </summary>
+        /// <param name="obj">
+        /// The object to compare this instance to.
+        /// </param>
+        /// <param name="culture">
+        /// The culture to perform the comparison under. If <see langword="null"/>, the instances will be compared
+        /// using their preferred cultures. If an instance does not have a localization in the specified culture, the
+        /// comparison will use the localization in that instance's preferred culture.
+        /// </param>
+        /// <param name="options">
+        /// An optional value indicating the options that should be used by the comparison.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if this instance is equal to the object specified by <paramref name="obj"/> in the
+        /// culture specified by <paramref name="culture"/> according to the specified options; otherwise,
+        /// <see langword="false"/>.
+        /// </returns>
+        bool Equals(object? obj, CultureInfo? culture, CompareOptions options = CompareOptions.None);
+
+        /// <summary>
+        /// Returns the localization of this instance in the culture specified by <paramref name="culture"/>, or the
+        /// localization in the preferred culture if <paramref name="culture"/> is <see langword="null"/> or this
+        /// instance does not have a localization defined for that culture.
+        /// </summary>
+        /// <param name="culture">
+        /// When non-<see langword="null"/>, the culture of the localization to return. If this instance does not have
+        /// a localization in the specified culture, the localization in the preferred culture will be used. When
+        /// <see langword="null"/>, the preferred culture will be used.
+        /// </param>
+        /// <returns>
+        /// If <paramref name="culture"/> is non-<see langword="null"/> and this instance has a localization in the
+        /// specified culture, the localization of this instance in the specified culture. If
+        /// <paramref name="culture"/> is non-<see langword="null"/> and this instance does not have a localization in
+        /// the specified culture, the localization of this instance in the preferred culture. If
+        /// <paramref name="culture"/> is <see langword="null"/>, the localization of this instance in the preferred
+        /// culture.
+        /// </returns>
+        string ToString(CultureInfo? culture);
+    }
+}
---
+++ src/Drexel.Loggers/LocalizableString.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Drexel.Loggers
+{
+    public abstract class LocalizableString<TDerived> : ILocalizableString<LocalizableString<TDerived>, TDerived>
+        where TDerived : LocalizableString<TDerived>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocalizableString{TDerived}"/> class.
+        /// </summary>
+        /// <param name="invariantLocalization">
+        /// The localization in the invariant culture.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="invariantLocalization"/> is <see langword="null"/>.
+        /// </exception>
+        private protected LocalizableString(string invariantLocalization)
+        {
+            if (invariantLocalization is null)
+            {
+                throw new ArgumentNullException(nameof(invariantLocalization));
+            }
+
+            this.PreferredCulture = CultureInfo.InvariantCulture;
+            this.Localizations = ReadOnlyDictionary.Create(
+                new Dictionary<CultureInfo, string>(1)
+                {
+                    [CultureInfo.InvariantCulture] = invariantLocalization,
+                });
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocalizableString{TDerived}"/> class.
+        /// </summary>
+        /// <param name="localization">
+        /// The localization in the culture specified by <paramref name="culture"/>.
+        /// </param>
+        /// <param name="culture">
+        /// The culture of the localization.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="localization"/> or <paramref name="culture"/> is <see langword="null"/>.
+        /// </exception>
+        private protected LocalizableString(string localization, CultureInfo culture)
+        {
+            if (localization is null)
+            {
+                throw new ArgumentNullException(nameof(localization));
+            }
+
+            this.PreferredCulture = culture ?? throw new ArgumentNullException(nameof(culture));
+            this.Localizations = ReadOnlyDictionary.Create(
+                new Dictionary<CultureInfo, string>(1)
+                {
+                    [culture] = localization,
+                });
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LocalizableString{TDerived}"/> class.
+        /// </summary>
+        /// <param name="preferredCulture">
+        /// The preferred culture.
+        /// </param>
+        /// <param name="localizations">
+        /// The localizations. The supplied dictionary must contain a localization for the culture specified by
+        /// <paramref name="localizations"/>.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="localizations"/> or <paramref name="preferredCulture"/> is
+        /// <see langword="null"/>.
+        /// </exception>
+        private protected LocalizableString(
+            CultureInfo preferredCulture,
+            IReadOnlyDictionary<CultureInfo, string> localizations)
+        {
+            this.PreferredCulture = preferredCulture ?? throw new ArgumentNullException(nameof(preferredCulture));
+            this.Localizations = ReadOnlyDictionary.Create(
+                localizations ?? throw new ArgumentNullException(nameof(localizations)));
+        }
+
+        public CultureInfo PreferredCulture { get; }
+
+        public IReadOnlyDictionary<CultureInfo, string> Localizations { get; }
+
+        /// <summary>
+        /// Returns a value indicating whether <paramref name="left"/> and <paramref name="right"/> are both
+        /// <see langword="null"/>, or have the string in their preferred cultures.
+        /// </summary>
+        /// <param name="left">
+        /// The left value.
+        /// </param>
+        /// <param name="right">
+        /// The right value.
+        /// /param>
+        /// <returns>
+        /// <see langword="true"/> if <paramref name="left"/> and <paramref name="right"/> are both
+        /// <see langword="null"/>, or have the same string in their preferred cultures; otherwise,
+        /// <see langword="false"/>.
+        /// </returns>
+        public static bool operator ==(LocalizableString<TDerived>? left, LocalizableString<TDerived>? right)
+        {
+            if (left is null)
+            {
+                return right is null;
+            }
+            else
+            {
+                return left.Equals(right);
+            }
+        }
+
+        /// <summary>
+        /// Returns a value indicating whether exactly one of <paramref name="left"/> and <paramref name="right"/> is
+        /// <see langword="null"/>, or <paramref name="left"/> and <paramref name="right"/> do not have the same
+        /// string in their preferred cultures.
+        /// </summary>
+        /// <param name="left">
+        /// The left value.
+        /// </param>
+        /// <param name="right">
+        /// The right value.
+        /// </param>
+        /// <returns>
+        /// <see langword="true"/> if exactly one of <paramref name="left"/> and <paramref name="right"/> is
+        /// <see langword="null"/>, or <paramref name="left"/> and <paramref name="right"/> have the same string in
+        /// their preferred cultures; otherwise, <see langword="false"/>.
+        /// </returns>
+        public static bool operator !=(LocalizableString<TDerived>? left, LocalizableString<TDerived>? right)
+        {
+            if (left is null)
+            {
+                return !(right is null);
+            }
+            else
+            {
+                return !left.Equals(right);
+            }
+        }
+
+        public int CompareTo(TDerived? other, CultureInfo? culture, CompareOptions options = CompareOptions.None)
+        {
+            if (other is null)
+            {
+                return 1;
+            }
+
+            string left = this.ToString(culture);
+            string right = other.ToString(culture);
+
+            return (culture ?? this.PreferredCulture).CompareInfo.Compare(left, right, options);
+        }
+
+        public int CompareTo(TDerived? other) => this.CompareTo(other, null);
+
+        public bool Equals(TDerived? other, CultureInfo? culture, CompareOptions options = CompareOptions.None)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            string left = this.ToString(culture);
+            string right = other.ToString(culture);
+
+            return (culture ?? this.PreferredCulture).CompareInfo.Compare(left, right, options) == 0;
+        }
+
+        public bool Equals(TDerived? other) => this.Equals(other, null);
+
+        public bool Equals(object? obj, CultureInfo? culture, CompareOptions options = CompareOptions.None)
+        {
+            if (obj is TDerived other)
+            {
+                return this.Equals(other, culture, options);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is TDerived other)
+            {
+                return this.Equals(other, null);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode() => this.ToString().GetHashCode();
+
+        public string ToString(CultureInfo? culture)
+        {
+            if (culture is null)
+            {
+                return this.Localizations[this.PreferredCulture];
+            }
+            else if (this.Localizations.TryGetValue(culture, out string value))
+            {
+                return value;
+            }
+            else
+            {
+                return this.Localizations[this.PreferredCulture];
+            }
+        }
+
+        public override string ToString() => this.ToString(null);
+    }
+}
